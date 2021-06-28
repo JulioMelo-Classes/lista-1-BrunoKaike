6c9f48e9df421ea1b9c055116170d70c5e2088bd
BrunoKaike <brunokaike2014@gmail.com>
2021-06-28 00:18:27 +0000

    Fibonacci

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -4,6 +4,29 @@ std::vector<unsigned int> fib_below_n( unsigned int n )
 {
     // TODO: adicione o seu código aqui.
 
+    std::vector<unsigned int> v;
+
+    int x = 0;
+
+    do{
+
+      if(x < 2 && 1 < n){
+
+        v.push_back(1);
+
+      } else if(x >= 2 && v[x-2]+v[x-1] < n) {
+        v.push_back(v[x-2]+v[x-1]);
+
+      } else {
+
+        break;
+
+      }
+
+      x++;
+
+    }while(v[x-1] < n);
+
     // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
-    return std::vector<unsigned int>{};
+    return v;
 }
