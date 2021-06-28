e82390f7a9124862b19016d9d7f1e89add3978f6
Bruno Kaike <brunokaike2014@gmail.com>
2021-06-28 01:36:50 +0000

    Filter

--- filter/src/function.cpp
+++ filter/src/function.cpp
@@ -12,10 +12,39 @@
  * @param last Pointer just past the last valid value of the range we want to filter.
  * @return a pointer to the new 'logical' end of the array.
  */
-int * filter( int * first, int * last )
+
+int * filter(int * first, int * last )
 {
     // TODO: coloque aqui o seu código-solução.
-    
+  
+    auto i=first;
+
+    for(;i!=last;){
+
+       //cout << *i << endl;
+
+      if(*i<=0){
+
+        auto b=i;
+        while(b <=last-1){
+            int aux = *b;
+            *b = *(b+1);
+            *(b+1) = aux;
+            b++;
+
+        }
+
+        last--;
+
+      } else {
+
+        i++;
+
+      }
+
+    }
+
+    return last;
+
     // TODO: substitua o retorno de acordo com a solução. Isso é somente um STUB temporário.
-    return nullptr;
 }
