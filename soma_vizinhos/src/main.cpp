54384d5c06d98f6d44ca7eda68a163688507d964
BrunoKaike <brunokaike2014@gmail.com>
2021-06-28 00:10:12 +0000

    Soma vizinhos

--- soma_vizinhos/src/main.cpp
+++ soma_vizinhos/src/main.cpp
@@ -11,6 +11,44 @@ using std::endl;
 int main( void )
 {
     // TODO: Adicione seu código aqui. 
+    int m, n;
+    long int result;
+
+    while(!cin.eof()){
+      
+      result = 0;
+
+      std::cin>>std::ws>>m;
+      std::cin>>std::ws>>n;
+
+      if(n > 0){
+
+        for(int x = 0; x < n; x++){
+          
+          result+= m;
+          m++;
+
+        }
+
+      } else if(n == 0){
+
+        result = m;
+
+      } else{
+
+        for(int x = 0; x < (n*-1); x++){
+          
+          result+= m;
+          m--;
+
+        }
+
+
+      }
+
+      std::cout << result << "\n";
+
+    }
 
     return 0;
 }
